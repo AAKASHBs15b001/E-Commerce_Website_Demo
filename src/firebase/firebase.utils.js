31d13c3af7f155af565a6f49ac111aa8846a76c1
AAKASHBs15b001 <68820655+AAKASHBs15b001@users.noreply.github.com>
2021-06-21 23:29:13 +0530

    commit after firebase utils change
    commit after firebase utils change

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+   apiKey: "AIzaSyB9ePW2k-yc133qM6-DF67RMfdgpDks9KI",
+    authDomain: "crwn-clothing-demo-642f7.firebaseapp.com",
+    projectId: "crwn-clothing-demo-642f7",
+    storageBucket: "crwn-clothing-demo-642f7.appspot.com",
+    messagingSenderId: "1037375038547",
+    appId: "1:1037375038547:web:ca6a8e15b119f3b16b92a6",
+    measurementId: "G-XQE1LX8TTX"
 };
 
 firebase.initializeApp(config);
